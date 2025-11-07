a92e6f0c3e76ea37ab8ace81e2e66a7d77765697
rflrytdzn <kuhaku.blank.rd@gmail.com>
2025-11-07 18:11:44 +0800

    hide secrets

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get, set, update } from "firebase/database";
+import { getDatabase } from "firebase/database";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDjjWre9ycMfhz6AjFNA48Uh7wyXtfi9do",
-  authDomain: "test-a0bfb.firebaseapp.com",
-  databaseURL: "https://test-a0bfb-default-rtdb.firebaseio.com",
-  projectId: "test-a0bfb",
-  storageBucket: "test-a0bfb.firebasestorage.app",
-  messagingSenderId: "884937404858",
-  appId: "1:884937404858:web:958c0c6e41d36a502bed83",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const db = getDatabase(app);
-export { ref, get, set, update };
